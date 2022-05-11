3e0bd06a382aa18d6ced429470706678ee89d41c
Mustafa Çelik <mustik1518@gmail.com>
2022-05-11 23:38:32 +0300

    proje %98

--- mobilyargi/lib/models/UsersInfo.dart
+++ mobilyargi/lib/models/UsersInfo.dart
@@ -1,18 +1,1 @@
-import 'package:json_annotation/json_annotation.dart';
 
-@JsonSerializable(fieldRename: FieldRename.snake)
-class UsersInfo {
-  String mail;
-  String nickname;
-  bool isAdmin;
-  List<String>? likedsubjects;
-  List<String>? comments;
-  UsersInfo({
-    required this.mail,
-    required this.nickname,
-    required this.isAdmin,
-  });
-  factory UsersInfo.fromJson(Map<String, dynamic> json) =>
-      _$UsersInfoFromJson(json);
-  Map<String, dynamic> toJson() => _$UsersInfoToJson(this);
-}
--- mobilyargi/lib/pages/SubjectPage/SubjectScreen.dart
+++ mobilyargi/lib/pages/SubjectPage/SubjectScreen.dart
@@ -1,11 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:mobilyargi/pages/HomePage/Homepage.dart';
+import 'package:intl/intl.dart';
 
 class SubjectScreen extends StatefulWidget {
-  SubjectScreen(
-    this.SubjectTitle, {
+  final String _subjectTitle;
+  const SubjectScreen(
+    this._subjectTitle, {
     Key? key,
   }) : super(key: key);
-  String? SubjectTitle;
   @override
   State<SubjectScreen> createState() => _SubjectScreenState();
 }
@@ -14,11 +17,15 @@ class _SubjectScreenState extends State<SubjectScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: getBody(context),
+      body: getBody(context, widget._subjectTitle),
     );
   }
 
-  getBody(BuildContext context) {
+  getBody(BuildContext context, String subjectTitle) {
+    CollectionReference subjectCollection =
+        FirebaseFirestore.instance.collection('Subjects');
+    final TextEditingController _commentcontroller = TextEditingController();
+    var subjectInfo = subjectCollection.doc(subjectTitle);
     return SafeArea(
       child: SingleChildScrollView(
         child: Container(
@@ -31,8 +38,114 @@ class _SubjectScreenState extends State<SubjectScreen> {
             ),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              StreamBuilder<DocumentSnapshot>(
+                  stream: subjectInfo.snapshots(),
+                  builder: (context, AsyncSnapshot asyncSnapshot) {
+                    // var response = await subjectInfo.get();
+                    //           var veri = response.data();
+                    if (asyncSnapshot.hasData) {
+                      DateTime dt =
+                          (asyncSnapshot.data['Date'] as Timestamp).toDate();
+                      String d24 = DateFormat('dd/MM/yyyy, HH:mm').format(dt);
+                      return Column(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Color.fromARGB(255, 228, 161, 156),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            child: Text('${asyncSnapshot.data['Title']}'),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 20,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Color.fromARGB(255, 228, 161, 156),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            child: Text('${asyncSnapshot.data['Index']}'),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 20,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 228, 161, 156),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                child: Text(
+                                    'Yazar....:${asyncSnapshot.data['Writer']}'),
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width / 15,
+                              ),
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 228, 161, 156),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                child: Text(
+                                    'Beğeni Sayısı...:${asyncSnapshot.data['NumberofLike']}'),
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width / 15,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 228, 161, 156),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5)),
+                                  ),
+                                  child: Text("Tarih..:" + d24.toString()),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 20,
+                          ),
+                          TextFormField(
+                            minLines: 6,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            controller: _commentcontroller,
+                            decoration: const InputDecoration(
+                                labelText: 'Yorum Yap',
+                                border: OutlineInputBorder(),
+                                hintText: 'Bu kararı beğendim 👍'),
+                          ),
+                          ElevatedButton(
+                            onPressed: (() {
+                              //TODO: Yorum ekleme işlemi
+                            }),
+                            child: const Text("Gönder"),
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Text("Hata oluştu.Sonra tekrar deneyiniz.");
+                    }
+                  }),
+              ElevatedButton(
+                onPressed: (() async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                }),
+                child: const Text("Anasayfa"),
+              ),
+            ],
           ),
         ),
       ),
